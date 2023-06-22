55e4d4a0f38feceea14d318fffbde8c93a40e0b3
zaid.mukaddam <zaidaltafmukaddam@gmail.com>
2023-06-22 14:37:36 +0530

    Comment out unused dropdown menu item

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -49,7 +49,7 @@ export function UserMenu({ user }: UserMenuProps) {
             <div className="text-xs text-zinc-500">{user?.email}</div>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
+          {/* <DropdownMenuItem asChild>
             <a
               href="https://vercel.com"
               target="_blank"
@@ -59,7 +59,7 @@ export function UserMenu({ user }: UserMenuProps) {
               Vercel Homepage
               <IconExternalLink className="ml-auto h-3 w-3" />
             </a>
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
           <DropdownMenuItem
             onClick={() =>
               signOut({
